7da51ade77f5fe4d52cb96d6abbda6ed3ed498d4
bvisible <info@bvisible.ch>
2024-01-29 11:52:27 +0100

    update to get a real usable report on timesheet

--- hrms/hr/report/employee_hours_utilization_based_on_timesheet/employee_hours_utilization_based_on_timesheet.py
+++ hrms/hr/report/employee_hours_utilization_based_on_timesheet/employee_hours_utilization_based_on_timesheet.py
@@ -5,6 +5,7 @@
 import frappe
 from frappe import _
 from frappe.utils import flt, getdate
+from datetime import datetime, timedelta #//// added
 
 
 def execute(filters=None):
@@ -16,7 +17,12 @@ class EmployeeHoursReport:
 
 	def __init__(self, filters=None):
 		self.filters = frappe._dict(filters or {})
-
+		'''#//// added block
+		if self.filters.to_date:
+			date_obj = datetime.strptime(self.filters.to_date, "%Y-%m-%d")
+			date_obj = date_obj + timedelta(days=1)
+			self.filters.to_date = date_obj.strftime("%Y-%m-%d")
+		#////'''
 		self.from_date = getdate(self.filters.from_date)
 		self.to_date = getdate(self.filters.to_date)
 
@@ -139,8 +145,8 @@ class EmployeeHoursReport:
 					additional_filters += f" AND ttd.{field} = {self.filters.get(field)!r}"
 				else:
 					additional_filters += f" AND tt.{field} = {self.filters.get(field)!r}"
-
-		self.filtered_time_logs = frappe.db.sql(
+		#//// commented
+		'''self.filtered_time_logs = frappe.db.sql(
 			"""
 			SELECT tt.employee AS employee, ttd.hours AS hours, ttd.is_billable AS is_billable, ttd.project AS project
 			FROM `tabTimesheet Detail` AS ttd
@@ -153,12 +159,54 @@ class EmployeeHoursReport:
 		""".format(
 				self.filters.from_date, self.filters.to_date, additional_filters
 			)
+		)'''
+		#////
+
+		#//// added
+		self.filtered_time_logs = frappe.db.sql(
+			"""
+			SELECT tt.employee AS employee, ttd.hours AS hours, ttd.is_billable AS is_billable, ttd.project AS project,
+			ttd.from_time AS start_time, ttd.to_time AS end_time
+			FROM `tabTimesheet Detail` AS ttd
+			JOIN `tabTimesheet` AS tt
+				ON ttd.parent = tt.name
+			WHERE tt.employee IS NOT NULL
+			AND NOT (tt.start_date > '{1}' OR tt.end_date < '{0}')
+			AND ttd.from_time BETWEEN '{0}' AND DATE_ADD('{1}', INTERVAL 1 DAY)
+			{2}
+		""".format(
+				self.filters.from_date, self.filters.to_date, additional_filters
+			)
+			#AND (tt.start_date BETWEEN '{0}' AND '{1}' OR tt.end_date BETWEEN '{0}' AND '{1}')
 		)
+		'''data_list = [list(record) for record in self.filtered_time_logs]
+		for record in data_list:
+			from_date_datetime = datetime.strptime(self.filters.from_date, "%Y-%m-%d")
+			to_date_datetime = datetime.strptime(self.filters.to_date, "%Y-%m-%d")
+			to_date_datetime = to_date_datetime.replace(hour=23, minute=59, second=59)
+			frappe.neolog("record", record)
+			frappe.neolog(str(type(record[4])) + " " + str(type(from_date_datetime)) + " " + str(type(record[5])) + " " + str(type(to_date_datetime)))
+			frappe.neolog("{} <? {}".format(record[4], from_date_datetime))
+			frappe.neolog("{} >? {}".format(record[5], to_date_datetime))
+			if record[4] < from_date_datetime:
+				frappe.neolog("record[4] < from_date_datetime")
+				time_diff = from_date_datetime - record[4]
+				hours_diff = time_diff.total_seconds() / 3600
+				record[4] = from_date_datetime
+				record[1] = flt(record[1] - hours_diff, 2)
+			if record[5] > to_date_datetime:
+				frappe.neolog("record[5] > to_date_datetime")
+				time_diff = record[5] - to_date_datetime
+				hours_diff = time_diff.total_seconds() / 3600
+				record[5] = to_date_datetime
+				record[1] = flt(record[1] - hours_diff, 2)
+		self.filtered_time_logs = [tuple(record) for record in data_list]'''
+		#////
 
 	def generate_stats_by_employee(self):
 		self.stats_by_employee = frappe._dict()
 
-		for emp, hours, is_billable, project in self.filtered_time_logs:
+		for emp, hours, is_billable, project, start_time, end_time in self.filtered_time_logs: #//// added start_time, end_time
 			self.stats_by_employee.setdefault(emp, frappe._dict()).setdefault("billed_hours", 0.0)
 
 			self.stats_by_employee[emp].setdefault("non_billed_hours", 0.0)
@@ -179,6 +227,64 @@ class EmployeeHoursReport:
 	def calculate_utilizations(self):
 		TOTAL_HOURS = flt(self.standard_working_hours * self.day_span, 2)
 		for emp, data in self.stats_by_employee.items():
+			#//// added block
+			try:
+				employee = frappe.get_doc("Employee", emp)
+				holidays = []
+				if employee.holiday_list:
+					holiday_list = frappe.get_doc("Holiday List", employee.holiday_list)
+					holidays += [d.holiday_date for d in holiday_list.holidays]
+				company_default_holidays_list = frappe.db.get_value("Company", self.filters.company, "default_holiday_list")
+				if company_default_holidays_list:
+					company_holiday_list = frappe.get_doc("Holiday List", company_default_holidays_list)
+					holidays += [d.holiday_date for d in company_holiday_list.holidays]
+				sorted_degrees_by_date = sorted(employee.employment_degrees, key=lambda x: x.date)
+				#frappe.neolog("sorted_degrees_by_date", sorted_degrees_by_date)
+				split_date = self.to_date
+				index_start = None
+				percentage = 100
+				for idx, degree in enumerate(sorted_degrees_by_date):
+					frappe.neolog("degree.date({}) <= self.from_date ({}) = {}".format(degree.date, self.from_date, degree.date <= self.from_date))
+					#frappe.neolog("degree percentage = {}".format(degree.degree))
+					if degree.date <= self.from_date:
+						percentage = degree.degree
+						index_start = idx
+					else:
+						break
+				frappe.neolog("index_start = {}".format(index_start))
+				split_percentage = []
+				if index_start is not None:
+					split_from = self.from_date
+					for idx, degree in enumerate(sorted_degrees_by_date[index_start+1:]):
+						if self.to_date > degree.date:
+							split_percentage.append({"split_from": split_from, "split_to": degree.date - timedelta(days=1), "percentage": percentage})
+							percentage = degree.degree
+							split_from = degree.date
+						else:
+							break
+					split_percentage.append({"split_from": split_from, "split_to": self.to_date, "percentage": percentage})
+				else:
+					split_percentage.append({"split_from": self.from_date, "split_to": self.to_date, "percentage": percentage})
+				frappe.neolog("split_percentage", split_percentage)
+				#frappe.neolog("split_date = {}".format(split_date))
+				#frappe.neolog("self.to_date = {}".format(self.to_date))
+				TOTAL_HOURS = 0
+				for split in split_percentage:
+					current_date = split["split_from"]
+					working_days_count = 0
+					frappe.neolog("holidays", holidays)
+					while current_date <= split["split_to"]:
+						if current_date.weekday() < 5 and current_date not in holidays:  # Weekday is less than 5 for Mon-Fri
+							working_days_count += 1
+						current_date += timedelta(days=1)
+					#frappe.neolog("working_days_count {}".format(working_days_count))
+					#frappe.neolog("standard_working_hours {}".format(self.standard_working_hours))
+					TOTAL_HOURS += flt(working_days_count * self.standard_working_hours / 100 * split["percentage"], 2)
+					#frappe.neolog("TOTAL_HOURS {}".format(TOTAL_HOURS))
+					#frappe.neolog("supposed TOTAL_HOURS  {}".format(flt(working_days_count * self.standard_working_hours, 2)))
+			except Exception as e:
+				frappe.neolog("Error", e)
+			#////
 			data["total_hours"] = TOTAL_HOURS
 			data["untracked_hours"] = flt(TOTAL_HOURS - data["billed_hours"] - data["non_billed_hours"], 2)
 
