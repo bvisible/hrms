185efb9e53a69f57458c089f2952e65e2862d857
Jérémy Christillin <jeremy@neoservice.ai>
2026-02-21 17:55:13 +0100

    feat(payroll): add Swiss Lohnausweis Mapping child table

---
+++ hrms/payroll/doctype/swiss_lohnausweis_mapping/swiss_lohnausweis_mapping.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+# License: GNU General Public License v3. See license.txt
+
+from frappe.model.document import Document
+
+
+class SwissLohnausweisMapping(Document):
+	# Child table for mapping salary components to Lohnausweis positions
+	pass
